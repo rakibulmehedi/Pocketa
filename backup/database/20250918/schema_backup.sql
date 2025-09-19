6ca1bf8f369462e15a0f1b6ef7eee0f4593070a1
Rakibul Islam Mehedi <rakibulmehedi.dev@gmail.com>
2025-09-19 10:13:15 +0600

    chore: add backup files and configuration snapshots

---
+++ backup/database/20250918/schema_backup.sql
@@ -0,0 +1,14 @@
+-- Database Schema Backup
+-- Generated: $(date)
+
+-- Tables/Boxes
+-- transactions_box
+-- wallets_box  
+-- categories_box
+-- prefs_box
+
+-- Adapters
+-- TransactionAdapter
+-- WalletModelAdapter
+-- WalletTypeDtoAdapter
+-- CategoryModelAdapter
